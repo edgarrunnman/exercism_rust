d300685ce6510f9f0704cd8dcf2179717a927622
Edgar Runnman <edgar.runnman@lm.se>
2023-05-08 09:56:36 +0200

    armstrong

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -3,11 +3,10 @@ use std::collections::HashSet;
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let normalized_word = normalize_string(word);
     possible_anagrams
-        .iter()
+        .into_iter()
         .filter(|item| {
             normalize_string(item) == normalized_word && item.to_lowercase() != word.to_lowercase()
         })
-        .map(|item| *item)
         .collect()
 }
 fn normalize_string(input: &str) -> String {
--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -3,12 +3,14 @@
 
 #[derive(Debug)]
 pub struct Duration {
-    age: u64,
+    age: f64,
 }
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        Duration { age: s / 31557600 }
+        Duration {
+            age: s as f64 / 31557600 as f64,
+        }
     }
 }
 
@@ -29,11 +31,43 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    fn years_during(d: &Duration) -> f64 {
+        d.age as f64 / 0.2408467
+    }
+}
+impl Planet for Venus {
+    fn years_during(d: &Duration) -> f64 {
+        d.age as f64 / 0.61519726
+    }
+}
+impl Planet for Earth {
+    fn years_during(d: &Duration) -> f64 {
+        d.age as f64
+    }
+}
+impl Planet for Mars {
+    fn years_during(d: &Duration) -> f64 {
+        d.age as f64 / 1.8808158
+    }
+}
+impl Planet for Jupiter {
+    fn years_during(d: &Duration) -> f64 {
+        d.age as f64 / 11.862615
+    }
+}
+impl Planet for Saturn {
+    fn years_during(d: &Duration) -> f64 {
+        d.age as f64 / 29.447498
+    }
+}
+impl Planet for Uranus {
+    fn years_during(d: &Duration) -> f64 {
+        d.age as f64 / 84.016846
+    }
+}
+impl Planet for Neptune {
+    fn years_during(d: &Duration) -> f64 {
+        d.age as f64 / 164.79132
+    }
+}

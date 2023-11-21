de7953f0c9258ebb0b21c4f696f9e655520a8e73
Edgar <edgar.runnman@gmail.com>
2023-11-21 20:20:56 +0100

    minesweeper, high-scores

--- high-scores/src/lib.rs
+++ high-scores/src/lib.rs
@@ -1,30 +1,28 @@
 #[derive(Debug)]
-pub struct HighScores {
-    scores: Vec<u32>,
-    scores_ref: &'static [u32],
+pub struct HighScores<'a> {
+    scores: &'a [u32],
 }
 
-impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        HighScores {
-            scores: scores.to_vec(),
-            scores_ref: scores,
-        }
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
     }
 
     pub fn scores(&self) -> &[u32] {
-        unimplemented!("Return all the scores as a slice")
+        self.scores
     }
 
     pub fn latest(&self) -> Option<u32> {
-        unimplemented!("Return the latest (last) score")
+        self.scores.last().copied()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        unimplemented!("Return the highest score")
+        self.scores.iter().max().copied()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        unimplemented!("Return 3 highest scores")
+        let mut scores: Vec<u32> = self.scores.into_iter().map(|s| *s).collect();
+        scores.sort();
+        scores.into_iter().rev().take(3).collect()
     }
 }
--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -1,13 +1,46 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    let foo = minefield
-        .into_iter()
-        .map(|line| {
-            line.chars()
-                .into_iter()
-                .map(|symbol| symbol as u8)
-                .collect::<Vec<u8>>()
+    (0..minefield.len())
+        .map(|x| {
+            (0..minefield[x].len())
+                .map(|y| match minefield[x].as_bytes()[y] {
+                    42 => '*',
+                    _ => count_mines(minefield, x as i8, y as i8) as char,
+                })
+                .collect()
         })
-        .collect::<Vec<Vec<u8>>>();
-    println!("{:?}", foo);
-    Vec::new()
+        .collect()
+}
+
+fn count_mines(minefield: &[&str], index_x: i8, index_y: i8) -> u8 {
+    (-1..2 as i8)
+        .fold(None as Option<u8>, |acc, x| {
+            (-1..2 as i8).fold(acc, |mines, y| {
+                if is_mine(minefield, index_x + x, index_y + y) {
+                    add_one(mines)
+                } else {
+                    mines
+                }
+            })
+        })
+        .unwrap_or(32)
+}
+
+fn add_one(mines: Option<u8>) -> Option<u8> {
+    match mines {
+        Some(n) => Some(n + 1),
+        None => Some(49),
+    }
+}
+
+fn is_mine(minefield: &[&str], index_x: i8, index_y: i8) -> bool {
+    minefield
+        .iter()
+        .nth(index_x as usize)
+        .and_then(|line| {
+            line.bytes()
+                .into_iter()
+                .nth(index_y as usize)
+                .and_then(|char| Some(char == 42))
+        })
+        .unwrap_or(false)
 }
